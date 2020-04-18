2fbd7cd8d08e25587e53967bbc58c0ac990aacd3
igneoman <gasconignacio@gmail.com>
2020-04-18 22:26:42 +0200

    Modificaciones varias ventana secundaria (1)

--- src/Principal/GestorProveedores.java
+++ src/Principal/GestorProveedores.java
@@ -3,10 +3,16 @@ package Principal;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import javax.swing.JTextField;
+import javax.swing.JTextPane;
+import javax.swing.JScrollPane;
 
 public class GestorProveedores {
 
 	private JFrame frame;
+	private JTextField textField;
 
 	/**
 	 * Launch the application.
@@ -38,6 +44,28 @@ public class GestorProveedores {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel label = new JLabel("CIF del proveedor:");
+		label.setHorizontalAlignment(SwingConstants.RIGHT);
+		label.setBounds(10, 11, 135, 14);
+		frame.getContentPane().add(label);
+		
+		textField = new JTextField();
+		textField.setHorizontalAlignment(SwingConstants.CENTER);
+		textField.setColumns(10);
+		textField.setBounds(155, 8, 96, 20);
+		frame.getContentPane().add(textField);
+		
+		JLabel label_1 = new JLabel("Raz\u00F3n Social del Proveedor:");
+		label_1.setBounds(10, 36, 192, 14);
+		frame.getContentPane().add(label_1);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(10, 61, 320, 88);
+		frame.getContentPane().add(scrollPane);
+		
+		JTextPane textPane = new JTextPane();
+		scrollPane.setViewportView(textPane);
 	}
-
 }
