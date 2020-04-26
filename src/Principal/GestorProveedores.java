440856c4dd5da72b28740257e225cb2d8329404b
igneoman <gasconignacio@gmail.com>
2020-04-26 11:28:05 +0200

    Ventana secundaria habilitada como la primaria para hacer pruebas,

--- src/Principal/GestorProveedores.java
+++ src/Principal/GestorProveedores.java
@@ -8,6 +8,8 @@ import javax.swing.JTextField;
 import javax.swing.JTextPane;
 import javax.swing.JScrollPane;
 import javax.swing.JButton;
+
+import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
@@ -35,9 +37,9 @@ public class GestorProveedores {
 	
 
 	/**
-	 * Launch the application.
+	 * Launch the application. Deshabilitar después
 	 */
-	/*public static void main(String[] args) {
+	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
@@ -48,7 +50,7 @@ public class GestorProveedores {
 				}
 			}
 		});
-	}*/
+	}
 
 	/**
 	 * Create the application.
